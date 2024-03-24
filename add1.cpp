d3855a37e09731ac1d5cb4d2709c4990080306c6
kimaya <kimayamore@gmail.com>
2024-03-24 13:53:55 +0530

    add with multiply

---
+++ add1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b)
+{
+    cout<<a+b;
+    }
+    int multiply(int a,int b){
+        cout<<"\n"<<a*b;
+        }
+        int main()
+        {
+            int x,y,result;
+            cout<<"\n enter value for x;";
+            cin>>y
+            add(x,y);
+            multiply(x,y);
+            }
---
+++ add2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int addition(int a,int b)
+{
+    cout<<a+b;
+    }
+    int multiply(int a,int b){
+        cout<<"\n"<<a*b;
+        }
+        int main()
+        {
+            int x,y,result;
+            cout<<"\n enter value for x;";
+            cin>>x;
+            cout<<"\n enter value for y;";
+            cin>>y
+            <<<<<<<Head
+            addition(x,y);
+            =======
+            add(x,y);
+            >>>>>>>
+            multiply(x,y);
+            }
