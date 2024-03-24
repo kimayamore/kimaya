b7b30954aadda8c20ffe143813c1ec864fd3fed1
kimaya <kimayamore@gmail.com>
2024-03-24 13:32:22 +0530

    add with multiply

---
+++ prac1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b)
+{
+    cout<<a+b;
+}
+    int main()
+{
+        int x,y,result;
+        cout<<"\n enter value for x";
+        cin>>x;
+        cout<<"\n enter value for y:";
+        cin>>y;
+        add(x,y);
+}
+    
